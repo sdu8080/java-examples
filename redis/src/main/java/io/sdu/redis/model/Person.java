0946e0accb0f8a8923c6da8508e71c645d85ceb7
Sean Du <sdu8080@yahoo.com>
2018-01-31 16:13:10 -0800

    add more examples

--- redis/src/main/java/io/sdu/redis/model/Person.java
+++ redis/src/main/java/io/sdu/redis/model/Person.java
@@ -1,13 +1,18 @@
 package io.sdu.redis.model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @RedisHash("persons")
-public @Data @AllArgsConstructor @NoArgsConstructor class Person {
-  @Id private Long id;
+public class Person {
+  @Id
+  private Long id;
   private String name;
 }
